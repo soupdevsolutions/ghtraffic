916b504aaec9c3533b2562b1fd237b694629b6b9
mirch <mirceateodor.oprea@gmail.com>
2024-05-27 10:34:51 +0300

    Add CI/CD pipelines

---
+++ scripts/build.py
@@ -0,0 +1,17 @@
+# builds the lambda functions and copies them to the infrastructure folder
+import os
+import sys
+
+commands = [
+    "pip3 install cargo-lambda",
+    "cargo lambda build --release --output-format zip"
+]
+os.system("; ".join(commands))
+
+functions = [name for name in os.listdir("target/lambda/")]
+
+commands.append("mkdir -p infrastructure/data/lambdas")
+
+for function in functions:
+    commands.append(f"cp target/lambda/{function}/bootstrap.zip infrastructure/data/lambdas/{function}.zip")
+os.system("; ".join(commands))
---
+++ src/bins/hello_world.rs
@@ -0,0 +1,18 @@
+use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
+
+async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
+    let message = "Hello, world!";
+
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body(message.into())
+        .map_err(Box::new)?;
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing::init_default_subscriber();
+    run(service_fn(function_handler)).await
+}
