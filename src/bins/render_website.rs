dadaf7176576bd632ecd4f4b4e765e652e78eefb
mirch <mirceateodor.oprea@gmail.com>
2024-06-10 23:18:54 +0300

    Test rendering a different response

--- src/bins/render_website.rs
+++ src/bins/render_website.rs
@@ -1,18 +1,34 @@
-use lambda_http::{run, service_fn, tracing, Body, Error, Request, Response};
+use ghtraffic::github::GithubClient;
+use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
 
-async fn handler(_event: Request) -> Result<Response<Body>, Error> {
-
+async fn handler(event: Request) -> anyhow::Result<Response<Body>> {
     let client_id = std::env::var("GH_CLIENT_ID").expect("GH_CLIENT_ID is not set"); 
+    let code = event.query_string_parameters().first("code").map(|v| v.to_string());
+
+    let mut body = format!(r#"
+        <body>
+            <h1>Welcome to ghTraffic by soup.dev</h1>
+            <a href="https://github.com/login/oauth/authorize?client_id={client_id}">Login with GitHub</a>
+        </body>
+    "#);
+
+    if let Some(code) = code {
+        let client = GithubClient::new();
+        let response = client.exchange_code(code).await?;
+        body = format!(r#"
+            <body>
+                <h1>Successfully authenticated with GitHub</h1>
+                <p>Access Token: {}</p>
+            </body> 
+        "#, response.access_token);
+    }
 
     let data = format!(r#"
     <html>
         <head>
             <title>ghTraffix by soup.dev</title>
         </head>
-        <body>
-            <h1>Welcome to ghTraffic by soup.dev</h1>
-            <a href="https://github.com/login/oauth/authorize?client_id={client_id}">Login with GitHub</a>
-        </body>
+        {body}
     </html>
     "#);
 
