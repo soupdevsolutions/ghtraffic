5681ca1378c99e3060ea0c4b8737742a1c211fb9
mirch <mirceateodor.oprea@gmail.com>
2024-06-12 23:01:46 +0300

    Create bucket and upload files from CD

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,7 @@
 output "api_gateway_url" {
   value = aws_apigatewayv2_stage.api_stage.invoke_url
 }
+
+output "templates_bucket_name" {
+  value = aws_s3_bucket.htmx_templates.bucket
+}
---
+++ infrastructure/s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "htmx_templates" {
+  bucket = "ghstats-htmx-templates"
+}
