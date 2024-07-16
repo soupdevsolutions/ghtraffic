1960a48ae5d927ead1d6ecb7fd043814ff6ee886
mirch <mirceateodor.oprea@gmail.com>
2024-07-16 23:27:44 +0300

    Add provider to validation resource

--- infrastructure/acm.tf
+++ infrastructure/acm.tf
@@ -5,6 +5,7 @@ resource "aws_acm_certificate" "ghtraffic" {
 }
 
 resource "aws_acm_certificate_validation" "ghtraffic" {
+  provider                = aws.us
   certificate_arn         = aws_acm_certificate.ghtraffic.arn
   validation_record_fqdns = [for record in aws_route53_record.ghtraffic : record.fqdn]
 }
