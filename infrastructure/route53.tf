22064682c2fa470510f56b0cefeb6b2b31e2b780
mirch <mirceateodor.oprea@gmail.com>
2024-07-16 22:06:20 +0300

    Fix zone reference

--- infrastructure/route53.tf
+++ infrastructure/route53.tf
@@ -1,11 +1,11 @@
 data "aws_route53_zone" "ghtraffic" {
-  name = "www.ghtraffic.com"
+  name = "ghtraffic.com"
 }
 
 resource "aws_route53_record" "ghtraffic" {
   name    = aws_api_gateway_domain_name.ghtraffic.domain_name
   type    = "A"
-  zone_id = aws_route53_zone.ghtraffic.id
+  zone_id = data.aws_route53_zone.ghtraffic.id
 
   alias {
     evaluate_target_health = true
