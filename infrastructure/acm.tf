3292048313d041dcde3aaa68a15db2505dc149ee
mirch <mirceateodor.oprea@gmail.com>
2024-07-16 22:24:32 +0300

    Fix Route53/ACM code

--- infrastructure/acm.tf
+++ infrastructure/acm.tf
@@ -5,4 +5,5 @@ resource "aws_acm_certificate" "ghtraffic" {
 
 resource "aws_acm_certificate_validation" "ghtraffic" {
   certificate_arn = aws_acm_certificate.ghtraffic.arn
+  validation_record_fqdns = [for record in aws_route53_record.ghtraffic : record.fqdn]
 }
--- infrastructure/route53.tf
+++ infrastructure/route53.tf
@@ -1,15 +1,21 @@
 data "aws_route53_zone" "ghtraffic" {
-  name = "ghtraffic.com"
+  name         = "ghtraffic.com"
+  private_zone = false
 }
 
 resource "aws_route53_record" "ghtraffic" {
-  name    = aws_api_gateway_domain_name.ghtraffic.domain_name
-  type    = "A"
-  zone_id = data.aws_route53_zone.ghtraffic.id
-
-  alias {
-    evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.ghtraffic.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.ghtraffic.cloudfront_zone_id
+  for_each = {
+    for dvo in aws_acm_certificate.ghtraffic.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
   }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.ghtraffic.zone_id
 }
