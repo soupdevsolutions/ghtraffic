9fa4b6d7505f7e360e6d3a9ccaa056c4b9923245
mirch <mirceateodor.oprea@gmail.com>
2024-07-17 10:56:13 +0300

    Move ACM resources..back to eu-west-1?

--- infrastructure/acm.tf
+++ infrastructure/acm.tf
@@ -1,11 +1,9 @@
 resource "aws_acm_certificate" "ghtraffic" {
-  provider          = aws.us
   domain_name       = "ghtraffic.com"
   validation_method = "DNS"
 }
 
 resource "aws_acm_certificate_validation" "ghtraffic" {
-  provider                = aws.us
   certificate_arn         = aws_acm_certificate.ghtraffic.arn
   validation_record_fqdns = [for record in aws_route53_record.ghtraffic : record.fqdn]
 }
